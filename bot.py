c1f6813b688bfe3b583fa80ca2bb2130049ff86c
YMorice <yann.morice@audencia.com>
2026-03-10 11:31:08 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -13,6 +13,16 @@ print("KEY:", os.getenv("PAPER_APCA_API_KEY_ID").strip())
 print("SECRET:", os.getenv("PAPER_APCA_API_SECRET_KEY").strip())
 print("ALL ENV KEYS:", [k for k in os.environ.keys() if 'APCA' in k])
 
+r = requests.get(
+    "https://paper-api.alpaca.markets/v2/account",
+    headers={
+        "APCA-API-KEY-ID": API_KEY,
+        "APCA-API-SECRET-KEY": API_SECRET,
+    }
+)
+print(f"Status: {r.status_code}")
+print(f"Body: {r.text}")
+
 
 #---------------------------
 # API 
